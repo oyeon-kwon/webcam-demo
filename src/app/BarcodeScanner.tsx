5a36ed49ff5d3c6fac7bb812d83f755d0ba6b420
oyeon-kwon <ohyeon.kwon@flask.global>
2024-08-26 16:20:21 +0900

    add css

--- src/app/BarcodeScanner.tsx
+++ src/app/BarcodeScanner.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useZxing } from "react-zxing";
+import "./BarcodeScanner.css";
 
 export const BarcodeScanner = () => {
   const [result, setResult] = useState("");
@@ -19,7 +20,7 @@ export const BarcodeScanner = () => {
 
   return (
     <>
-      <video ref={ref} />
+      <video ref={ref} className="barcode_wrapper" />
       <p>
         <span>Last result:</span>
         <span>{result}</span>
